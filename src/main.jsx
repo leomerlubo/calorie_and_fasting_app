f83b948eea2a14b569e310cf041bed1adc5981a9
/ L e e <64281675+leomerlubo@users.noreply.github.com>
2026-08-22 09:18:26 +0800

    Use compact wellness test UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css';
+import TestApp from './TestApp.jsx';
+import './test.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TestApp />
   </React.StrictMode>,
 );
